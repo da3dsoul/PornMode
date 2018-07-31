704df9290447af4356189e35b10b9c60dda852a6
da3dsoul <da3dsoul@gmail.com>
2018-07-31 17:12:49 -0400

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace PornMode 
+{ 
+
+    [StructLayout(LayoutKind.Sequential)] 
+    public struct DEVMODE 
+    { 
+       [MarshalAs(UnmanagedType.ByValTStr,SizeConst=32)]
+       public string dmDeviceName;
+
+       public short  dmSpecVersion;
+       public short  dmDriverVersion;
+       public short  dmSize;
+       public short  dmDriverExtra;
+       public int    dmFields;
+       public int    dmPositionX;
+       public int    dmPositionY;
+       public int    dmDisplayOrientation;
+       public int    dmDisplayFixedOutput;
+       public short  dmColor;
+       public short  dmDuplex;
+       public short  dmYResolution;
+       public short  dmTTOption;
+       public short  dmCollate;
+
+       [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 32)]
+       public string dmFormName;
+
+       public short  dmLogPixels;
+       public short  dmBitsPerPel;
+       public int    dmPelsWidth;
+       public int    dmPelsHeight;
+       public int    dmDisplayFlags;
+       public int    dmDisplayFrequency;
+       public int    dmICMMethod;
+       public int    dmICMIntent;
+       public int    dmMediaType;
+       public int    dmDitherType;
+       public int    dmReserved1;
+       public int    dmReserved2;
+       public int    dmPanningWidth;
+       public int    dmPanningHeight;
+    }; 
+
+    class NativeMethods 
+    { 
+        [DllImport("user32.dll")] 
+        public static extern int EnumDisplaySettings(string deviceName, int modeNum, ref DEVMODE devMode); 
+        [DllImport("user32.dll")] 
+        public static extern int ChangeDisplaySettings(ref DEVMODE devMode, int flags); 
+        [DllImport("user32.dll", EntryPoint = "#2507")]
+        public static extern bool SetAutoRotation(bool bEnable);
+
+
+        public const int ENUM_CURRENT_SETTINGS = -1; 
+        public const int CDS_UPDATEREGISTRY = 0x01; 
+        public const int CDS_TEST = 0x02; 
+        public const int DISP_CHANGE_SUCCESSFUL = 0; 
+        public const int DISP_CHANGE_RESTART = 1; 
+        public const int DISP_CHANGE_FAILED = -1;
+        public const int DMDO_DEFAULT = 0;
+        public const int DMDO_90 = 1;
+        public const int DMDO_180 = 2;
+        public const int DMDO_270 = 3;
+    } 
+
+
+
+    public static class PrmaryScreenResolution 
+    {
+        public static void Main(string[] args)
+        {
+            ChangeResolution();
+        }
+
+        public static string ChangeResolution() 
+        { 
+
+            DEVMODE dm = GetDevMode(); 
+
+            if (0 != NativeMethods.EnumDisplaySettings(null, NativeMethods.ENUM_CURRENT_SETTINGS, ref dm)) 
+            {
+
+                // swap width and height
+                int temp = dm.dmPelsHeight;
+                dm.dmPelsHeight = dm.dmPelsWidth;
+                dm.dmPelsWidth = temp;
+
+                // determine new orientation based on the current orientation
+                switch(dm.dmDisplayOrientation)
+                {
+                    case NativeMethods.DMDO_DEFAULT:
+                        //dm.dmDisplayOrientation = NativeMethods.DMDO_270;
+                        //2016-10-25/EBP wrap counter clockwise
+                        NativeMethods.SetAutoRotation(false);
+                        dm.dmDisplayOrientation = NativeMethods.DMDO_90;
+                        break;
+                    case NativeMethods.DMDO_270:
+                        NativeMethods.SetAutoRotation(true);
+                        dm.dmDisplayOrientation = NativeMethods.DMDO_180;
+                        break;
+                    case NativeMethods.DMDO_180:
+                        NativeMethods.SetAutoRotation(false);
+                        dm.dmDisplayOrientation = NativeMethods.DMDO_270;
+                        break;
+                    case NativeMethods.DMDO_90:
+                        NativeMethods.SetAutoRotation(true);
+                        dm.dmDisplayOrientation = NativeMethods.DMDO_DEFAULT;
+                        break;
+                    default:
+                        // unknown orientation value
+                        // add exception handling here
+                        break;
+                }
+
+
+                int iRet = NativeMethods.ChangeDisplaySettings(ref dm, NativeMethods.CDS_TEST); 
+
+                if (iRet == NativeMethods.DISP_CHANGE_FAILED) 
+                { 
+                    return "Unable To Process Your Request. Sorry For This Inconvenience."; 
+                } 
+                else 
+                { 
+                    iRet = NativeMethods.ChangeDisplaySettings(ref dm, NativeMethods.CDS_UPDATEREGISTRY); 
+                    switch (iRet) 
+                    { 
+                        case NativeMethods.DISP_CHANGE_SUCCESSFUL: 
+                            { 
+                                return "Success"; 
+                            } 
+                        case NativeMethods.DISP_CHANGE_RESTART: 
+                            { 
+                                return "You Need To Reboot For The Change To Happen.\n If You Feel Any Problem After Rebooting Your Machine\nThen Try To Change Resolution In Safe Mode."; 
+                            } 
+                        default: 
+                            { 
+                                return "Failed To Change The Resolution"; 
+                            } 
+                    } 
+
+                } 
+
+
+            } 
+            else 
+            { 
+                return "Failed To Change The Resolution."; 
+            } 
+        } 
+
+        private static DEVMODE GetDevMode() 
+        { 
+            DEVMODE dm = new DEVMODE(); 
+            dm.dmDeviceName = new String(new char[32]); 
+            dm.dmFormName = new String(new char[32]); 
+            dm.dmSize = (short)Marshal.SizeOf(dm); 
+            return dm; 
+        } 
+    } 
+}
